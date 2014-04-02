7807cad2bd38c85380c44b3baa30c4e93dab1a37
Talglys <yottagamer@hotmail.com>
2014-04-02 14:23:52 +0200

    Merging

--- code/fileconvert.py
+++ code/fileconvert.py
@@ -9,23 +9,27 @@ PRES_LOG_NAME = "pres" + str + ".csv"
 
 #Convert the text file to the appropriate file type
 
-fp = open(LOG_NAME)
-csvtp = open(TEMP_LOG_NAME, 'w')
-csvpp = open(PRES_LOG_NAME, 'w')
+try:
+##    fp = open(LOG_NAME)
+##    csvtp = open(TEMP_LOG_NAME, 'w')
+##    csvpp = open(PRES_LOG_NAME, 'w')
 
-##fp = open(LOG_DIR + '/' + LOG_NAME)
-##csvtp = open(LOG_DIR + '/' + TEMP_LOG_NAME, 'w')
-##csvpp = open(LOG_DIR + '/' + PRES_LOG_NAME, 'w')
+    fp = open(LOG_DIR + '/' + LOG_NAME)
+    csvtp = open(LOG_DIR + '/' + TEMP_LOG_NAME, 'w')
+    csvpp = open(LOG_DIR + '/' + PRES_LOG_NAME, 'w')
 
-csvtp.write("\"Timestamp\",\"Temperature\"\n")
-csvpp.write("\"Timestamp\",\"Pressure\"\n")
-for line in fp:
-    splitLine = line.partition(",")
-    if splitLine[0] == "ADC":
-        csvtp.write(splitLine[2].rstrip("\n"))
-    if splitLine[0] == "TWI":
-        csvpp.write(splitLine[2].rstrip("\n"))
-        
-csvpp.close()
-csvtp.close()
-fp.close()
+    csvtp.write("\"Timestamp\",\"Temperature\"\n")
+    csvpp.write("\"Timestamp\",\"Pressure\"\n")
+    for line in fp:
+        splitLine = line.partition(",")
+        if splitLine[0] == "ADC":
+            csvtp.write(splitLine[2].rstrip("\n"))
+        if splitLine[0] == "TWI":
+            csvpp.write(splitLine[2].rstrip("\n"))
+            
+    csvpp.close()
+    csvtp.close()
+    fp.close()
+    
+except IOError:
+    print("The file was not found.")
